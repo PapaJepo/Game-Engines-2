79dfc01642cec08484d5743bdb83cb4c9abb00e8
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-02-27 15:49:22 +0000

    Update Steering.cs

--- GameEngines2Assignment/Assets/Steering.cs
+++ GameEngines2Assignment/Assets/Steering.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Steering : MonoBehaviour
 {
     public float Mass = 15;
-    public float MaxVelocity = 3;
-    public float MaxForce = 15;
+    public float MaxVelocity = 6;
+    public float MaxForce = 20;
 
     private Vector3 velocity;
 
@@ -50,8 +50,5 @@ public class Steering : MonoBehaviour
         velocity = Vector3.ClampMagnitude(velocity + steering, MaxVelocity);
         transform.position += velocity * Time.deltaTime;
         transform.forward = velocity.normalized;
-
-        Debug.DrawRay(transform.position, velocity.normalized * 2, Color.green);
-        Debug.DrawRay(transform.position, desiredVelocity.normalized * 2, Color.magenta);
     }
 }
