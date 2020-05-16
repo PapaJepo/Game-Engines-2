7b11fd49d468a173d0c52b5c7a840971c7e6b2a7
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-05-16 14:47:40 +0100

    Update Steering.cs

--- GameEngines2Assignment/Assets/Steering.cs
+++ GameEngines2Assignment/Assets/Steering.cs
@@ -4,27 +4,49 @@ using UnityEngine;
 
 public class Steering : MonoBehaviour
 {
+    public GameObject Banshee;
+
     public float Mass = 15;
     public float MaxVelocity = 6;
     public float MaxForce = 20;
 
-    private Vector3 velocity;
+    public Vector3 velocity;
 
     public List<Transform> target;
     private int CurrentWaypoint;
+
+    public bool ship;
+
+    Vector3 EnemyTarget;
+    Vector3 offset;
+    Vector3 WorldTarget;
+
+    public Transform otherOffsetPos;
 
     // Start is called before the first frame update
     void Start()
     {
         CurrentWaypoint = 0;
         velocity = Vector3.zero;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateWaypoints();
-        Seek();
+
+       
+        if(ship == true)
+        {
+            OffsetPursue();
+        }
+        else if( ship == false)
+        {
+            UpdateWaypoints();
+            Seek();
+
+        }
 
     }
 
@@ -40,6 +62,7 @@ public class Steering : MonoBehaviour
 
     void Seek()
     {
+        
         var desiredVelocity = target[CurrentWaypoint].position - transform.position;
         desiredVelocity = desiredVelocity.normalized * MaxVelocity;
 
@@ -54,6 +77,28 @@ public class Steering : MonoBehaviour
 
     void OffsetPursue()
     {
+        offset = otherOffsetPos.position - Banshee.transform.position;
 
+        WorldTarget = Banshee.transform.TransformPoint(offset);
+        float dist = Vector3.Distance(WorldTarget, transform.position);
+        float time = dist / MaxVelocity;
+        EnemyTarget = WorldTarget + (Banshee.GetComponent<Steering>().velocity * time);
+
+    }
+
+    public Vector3 Arrive(Vector3 targetPos, float slowDown = 10f)
+    {
+        Vector3 targetDir = targetPos - transform.position;
+
+        float distance = targetDir.magnitude;
+        if(distance < 0.1f)
+        {
+            return Vector3.zero;
+        }
+        float ramped = MaxVelocity * (distance / slowDown);
+
+        float clamped = Mathf.Min(ramped, MaxVelocity);
+        Vector3 desiredVelocity = clamped * (targetDir / distance);
+        return desiredVelocity - velocity;
     }
 }
