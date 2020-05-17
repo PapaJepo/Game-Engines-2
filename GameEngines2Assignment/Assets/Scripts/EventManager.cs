65f201568fb7b5d9f6a9199d49023c47fee71778
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-05-17 16:54:49 +0100

    Working on Camera shots for space scene

---
+++ GameEngines2Assignment/Assets/Scripts/EventManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    [SerializeField]
+    private float time = 120f;
+    public List<GameObject> Cameras;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if( time < 110f && time > 100f)
+        {
+            Cameras[0].SetActive(false);
+            Cameras[1].SetActive(true);
+        }
+    }
+}
